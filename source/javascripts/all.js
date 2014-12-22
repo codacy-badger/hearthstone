c7dc3e2ae05e2de76fc5622f98149324b0c9e261
bergren2 <dan@heymrbass.com>
2014-12-22 01:34:25 -0600

    add loading bar
    Former-commit-id: cf6e28d3a481357c2b5bf7ca4b7c1cdfd7d01c32

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -14,7 +14,36 @@ function search (query) {
   }
 }
 
+var progress = function () {
+  var amtOfCards;
+  var $progress;
+  var cardsLoaded = 0;
+
+  return {
+    init: function () {
+      amtOfCards = document.getElementsByClassName('card').length;
+      $progress = $('#loading .progress');
+    },
+    update: function () {
+      cardsLoaded += 1;
+      var percent = Math.floor(cardsLoaded / amtOfCards * 100);
+      $progress.width(percent + '%');
+      if (percent === 100) {
+        this.hide();
+      }
+    },
+    hide: function () {
+      $progress.addClass('not-in-use');
+    },
+    show: function () {
+      $progress.removeClass('not-in-use');
+    }
+  };
+}();
+
 $(document).on('ready', function () {
+  progress.init();
+
   $('#search > input').on('keypress', function (e) {
     if(e.which === 13) {
       search($(this).val());
@@ -25,6 +54,7 @@ $(document).on('ready', function () {
     $this = $(this);
     $this.on('load', function () {
       $(this).parent().addClass('loaded');
+      progress.update();
     }).on('click', function () {
       var img = '<img src="' + $(this).data('src') + '" />';
       $('#lightbox').addClass('loaded').html(img).show();
